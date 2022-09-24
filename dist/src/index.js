5337906275f4b7672398a0a08bfa78f599a4192d
Cristiano Melo <cristianodaniel@gmail.com>
2022-09-24 17:33:33 -0300

    gogo

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../database/app"));
-const data_source_1 = require("../data-source");
+const app_1 = __importDefault(require("./database/app"));
+const data_source_1 = require("./data-source");
 data_source_1.AppDataSource.initialize().then(() => {
     app_1.default.listen(3030, () => {
         console.log('Server online on port :: http://localhost:3030');
