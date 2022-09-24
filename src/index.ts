394f791f90a091f352950943eda26d7af898d63f
Cristiano Melo <cristianodaniel@gmail.com>
2022-09-24 17:39:26 -0300

    bug fix da port do .env do fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import app from "./database/app";
 import {AppDataSource} from "./data-source";
 
+const port = process.env.PORT || 3030
 
 AppDataSource.initialize().then(()=>{
     app.listen(3030, ()=>{
-        console.log('Server online on port :: http://localhost:3030')
+        console.log('Server online on port :: http://localhost:'+port)
     })
 });
