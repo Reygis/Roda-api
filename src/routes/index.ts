547b6caf54e62001c35e88af81dfdd638c989df7
Cristiano Melo <cristianodaniel@gmail.com>
2022-09-24 15:06:55 -0300

    feat: create: authMiddleware validar autenticação.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,8 @@
+import { User } from './../entities/User';
 import { Router, Request, Response } from "express"
+import { LoginController } from "../controller/LoginController";
+import { UserController } from "../controller/UserController";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 
 const routes = Router()
@@ -6,5 +10,12 @@ const routes = Router()
 routes.get('/', (req:Request, res:Response)=>{
     res.send('testando')
 })
+
+routes.post("/user", new UserController().create) //Rota para criar Users
+routes.post("/login", new LoginController().login) // Rota de Login
+
+routes.use(authMiddleware); // todas as rotas abaixo deste Middleware precisarão de autenticação
+
+routes.get("/alluser", UserController.listAllUser) //lista all users;
 
 export default routes
