30236c599bff07a34c746f77931baf5ee198d18e
Cristiano Melo <cristianodaniel@gmail.com>
2022-09-30 20:27:47 -0300

    up: bugfix

--- dist/controller/BookController.js
+++ dist/controller/BookController.js
@@ -1,36 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BookController = void 0;
-const userRepository_1 = require("../repositories/userRepository");
 const bookRepository_1 = require("../repositories/bookRepository");
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 class BookController {
 }
 exports.BookController = BookController;
 _a = BookController;
 BookController.create = async (req, res) => {
-    var _b;
-    const { authorization } = req.headers;
-    if (!authorization) {
-        return;
-    }
-    const token = authorization.split(" ")[1];
-    let iduser;
-    try {
-        const jwtPayload = jsonwebtoken_1.default.verify(token, (_b = process.env.JWT_PASS) !== null && _b !== void 0 ? _b : "");
-        iduser = jwtPayload.userId;
-    }
-    catch (error) {
-        return res.status(401).send;
-    }
-    const users = await userRepository_1.userRepository.findOneBy({ iduser });
-    if (!users)
-        return res.status(404).json({ message: 'User not found' });
     const { name, genres, description, condition } = req.body;
+    const users = req.user;
     try {
         const book = bookRepository_1.bookRepository.create({ name, genres, description, condition, users });
         await bookRepository_1.bookRepository.save(book);
--- dist/routes/auth.js
+++ dist/routes/auth.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const LoginController_1 = require("../controller/LoginController");
 const router = (0, express_1.Router)();
-router.post("/login", LoginController_1.LoginController.login); // Rota de Login
+router.post("/", LoginController_1.LoginController.login); // Rota de Login
+router.get("/profile", LoginController_1.LoginController.getProfiles); // retorna informações do usuario
 exports.default = router;
