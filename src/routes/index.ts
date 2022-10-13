83a8df9f8df8c0c564a6ce7b16e4c57250cfc95c
Reygis <103973457+Reygis@users.noreply.github.com>
2022-10-13 15:06:54 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,5 @@
 
-import { Group } from '../entities/Group';
-import { User } from './../entities/User';
-import { Router, Request, Response } from "express"
-import { LoginController } from "../controller/LoginController";
-import { UserController } from "../controller/UserController";
-import { GroupController } from './../controller/GroupController';
-import { authMiddleware } from "../middleware/authMiddleware";
-
+import { Router } from "express"
 
 import user from './user'
 import auth from './auth'
@@ -14,21 +7,6 @@ import book from './book'
 import review from './review'
 
 const routes = Router()
-
-
-routes.get('/', (req:Request, res:Response)=>{
-    res.send('testando')
-})
-
-routes.post("/user", new UserController().create) //Rota para criar Users
-routes.post("/login", new LoginController().login) // Rota de Login
-routes.get("/group", GroupController.listAllGroups) //Rota de listagem de todos os grupos
-
-
-routes.use(authMiddleware); // todas as rotas abaixo deste Middleware precisarão de autenticação
-
-routes.get("/alluser", UserController.listAllUser) //lista all users;
-routes.post("/group", new GroupController().create)// Rota de criação de grupos
 
 routes.use('/login',auth)
 routes.use('/user', user)
